7a92a3fc4774acff159bf814ad5b461cacb94ca7
radx64 <radekteam@gmail.com>
2016-11-01 21:20:52 +0100

    Remove some code duplication

--- src/graphics/CameraRenderer.cpp
+++ src/graphics/CameraRenderer.cpp
@@ -8,8 +8,16 @@ namespace graphics
 {
 
 CameraRenderer::CameraRenderer(Camera* cameraToFollow) : camera_(cameraToFollow)
-{
+{}
 
+bool isRenderNeeded(SDL_Rect* coords, Camera* camera)
+{
+    if (coords->x + coords->w < 0) return false;
+    if (coords->y + coords->h < 0) return false;
+
+    if (coords->x - coords->w > camera->getW()) return false;
+    if (coords->y - coords->h > camera->getH()) return false; 
+    return true;    
 }
 
 void CameraRenderer::draw(SDL_Texture* texture, SDL_Rect* renderQuad)
@@ -17,13 +25,10 @@ void CameraRenderer::draw(SDL_Texture* texture, SDL_Rect* renderQuad)
     renderQuad->x -= camera_->getX();
     renderQuad->y -= camera_->getY();
 
-    if (renderQuad->x + renderQuad->w < 0) return;
-    if (renderQuad->y + renderQuad->h < 0) return;
-
-    if (renderQuad->x - renderQuad->w > camera_->getW()) return;
-    if (renderQuad->y - renderQuad->h > camera_->getH()) return;  
-
-    SDL_RenderCopy(Context::getSdlRenderer(), texture, nullptr, renderQuad);
+    if (isRenderNeeded(renderQuad, camera_))
+    {
+        SDL_RenderCopy(Context::getSdlRenderer(), texture, nullptr, renderQuad);
+    }
 }
 
 void CameraRenderer::draw(SDL_Texture* texture, SDL_Rect* renderQuad, const FlipFlags* flipflags)
@@ -31,32 +36,26 @@ void CameraRenderer::draw(SDL_Texture* texture, SDL_Rect* renderQuad, const Flip
     renderQuad->x -= camera_->getX();
     renderQuad->y -= camera_->getY();
 
-    if (renderQuad->x + renderQuad->w < 0) return;
-    if (renderQuad->y + renderQuad->h < 0) return;
-
-    if (renderQuad->x - renderQuad->w > camera_->getW()) return;
-    if (renderQuad->y - renderQuad->h > camera_->getH()) return; 
-    // TODO: get rid of above code duplication
-    SDL_RenderCopyEx(Context::getSdlRenderer(),
-        texture,
-        NULL,
-        renderQuad,
-        0,
-        NULL,
-        flipflags->getSDL());
+    if (isRenderNeeded(renderQuad, camera_))
+    {
+        SDL_RenderCopyEx(Context::getSdlRenderer(),
+            texture,
+            NULL,
+            renderQuad,
+            0,
+            NULL,
+            flipflags->getSDL());
+    }
 }
 
 void CameraRenderer::draw(SDL_Texture* texture, SDL_Rect* source, SDL_Rect* destination)
 {
     destination->x -= camera_->getX();
     destination->y -= camera_->getY();
-
-    if (destination->x + destination->w < 0) return;
-    if (destination->y + destination->h < 0) return;
-
-    if (destination->x - destination->w > camera_->getW()) return;
-    if (destination->y - destination->h > camera_->getH()) return; 
-    SDL_RenderCopy(Context::getSdlRenderer(), texture, source, destination);
+    if (isRenderNeeded(destination, camera_))
+    {
+        SDL_RenderCopy(Context::getSdlRenderer(), texture, source, destination);
+    }
 }
 
 }  // namespace graphics
